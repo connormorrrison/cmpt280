79cdbe77b3d3d4c171bb67d564cb4d2c707cd086
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-03-07 03:58:56 -0600

    Add components to Node file

--- assignments/a4q2/src/AVLNode280.java
+++ assignments/a4q2/src/AVLNode280.java
@@ -1,2 +1,27 @@
-public class AVLNode280 {
+public class AVLNode280<I extends Comparable<I>> {
+    I data;
+    AVLNode280<I> left, right;
+    int leftHeight, rightHeight;
+
+    public AVLNode280(I data) {
+        this.data = data;
+        this.leftHeight = 0;
+        this.rightHeight = 0;
+    }
+
+    public int getLeftHeight() {
+        return leftHeight;
+    }
+
+    public void setLeftHeight(int height) {
+        this.leftHeight = height;
+    }
+
+    public int getRightHeight() {
+        return rightHeight;
+    }
+
+    public void setRightHeight(int height) {
+        this.rightHeight = height;
+    }
 }
